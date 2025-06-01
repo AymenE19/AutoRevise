9a053b7a3fd4d26062314e3f3829a1820d515ab1
Achraf Mansari <contact@achrafmansari.com>
2025-06-01 21:29:55 +0100

    Added a function to print nested dictionaries

--- utils/logger.py
+++ utils/logger.py
@@ -4,3 +4,16 @@ import logging
 def setup_logger():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     return logging.getLogger(__name__)
+
+
+def print_nested_dict(d, indent=0):
+    """Recursively prints a nested dictionary with indentation."""
+    for key, value in d.items():
+        print('    ' * indent + str(key))
+        if isinstance(value, dict):
+            print_nested_dict(value, indent + 1)
+        elif isinstance(value, list):
+            for item in value:
+                print('    ' * (indent + 1) + str(item))
+        else:
+            print('    ' * (indent + 1) + str(value))
