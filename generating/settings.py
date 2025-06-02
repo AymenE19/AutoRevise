499bc8673522cea7627c4f7c315adc83dd5a109e
Achraf Mansari <contact@achrafmansari.com>
2025-06-02 06:00:51 +0100

    Added a settings.py file into the generating folder

---
+++ generating/settings.py
@@ -0,0 +1,15 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+# --- Constants and Configuration ---
+load_dotenv("../.env")  # Load environment variables once at the beginning
+
+MONGODB_URI = getenv("MONGODB_URI")
+MONGODB_DB_NAME = getenv("MONGODB_DB_NAME")
+
+GEMINI_API_URL = getenv("GEMINI_API_URL")
+GEMINI_API_KEY = getenv("GEMINI_API_KEY")
+GEMINI_MODEL_NAME = getenv("GEMINI_MODEL_NAME")
+
+ALLOWED_USER_ACTIONS = {"summary", "quiz", "mind-map"}
